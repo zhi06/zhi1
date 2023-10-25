7de2676762c67ede1a98c15f4c5884c8f9e7810f
Zhi06 <samhjh666@gmail.com>
2023-10-25 23:33:21 +0800

    v1

---
+++ index.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    homepage = "<h1>謝仁翔Python網頁</h1>"
+    homepage += "<a href=/mis>MIS</a><br>"
+    homepage += "<a href=/today>顯示日期時間</a><br>"
+    homepage += "<a href=/welcome?nick=謝仁翔>傳送使用者暱稱</a><br>"
+    homepage += "<a href=/about>仁翔簡介網頁</a><br>"
+    return homepage
+
+
+@app.route("/mis")
+def course():
+    return "<h1>資訊管理導論</h1>"
+
+@app.route("/today")
+def today():
+    now = datetime.now()
+    return render_template("today.html",datetime = str(now))
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/welcome", methods=["GET", "POST"])
+def welcome():
+    user = request.values.get("nick")
+    return render_template("welcome.html", name=user)
+
+
+#if __name__ == "__main__":
+#    app.run()
