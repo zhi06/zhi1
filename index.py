3395df2fd7b37282157225a274a4a1643db78d9f
Zhi06 <samhjh666@gmail.com>
2023-11-13 23:41:19 +0800

    V3

--- index.py
+++ index.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template,request
 from datetime import datetime
 
 app = Flask(__name__)
@@ -31,6 +31,15 @@ def welcome():
     user = request.values.get("nick")
     return render_template("welcome.html", name=user)
 
+@app.route("/account", methods=["GET", "POST"])
+def account():
+    if request.method == "POST":
+        user = request.form["user"]
+        pwd = request.form["pwd"]
+        result = "您輸入的帳號是：" + user + "; 密碼為：" + pwd 
+        return result
+    else:
+        return render_template("account.html")
 
 #if __name__ == "__main__":
 #    app.run()
