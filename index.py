89dd5e9d96a5fd1964be7fa04ccf99c206d1e1a9
Zhi06 <samhjh666@gmail.com>
2023-11-22 21:37:49 +0800

    v23

--- index.py
+++ index.py
@@ -1,3 +1,8 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
 from flask import Flask, render_template, request
 from datetime import datetime
 import requests
