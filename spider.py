8b997642cf644ed05ccdc6e09b084cad585af689
Zhi06 <samhjh666@gmail.com>
2023-11-22 21:07:29 +0800

    v13

--- spider.py
+++ spider.py
@@ -1,13 +1,15 @@
 import requests
 from bs4 import BeautifulSoup
+
 url = "https://www1.pu.edu.tw/~tcyang/course.html"
 Data = requests.get(url)
 Data.encoding = "utf-8"
+
 #print(Data.text)
+
 sp = BeautifulSoup(Data.text, "html.parser")
-result=sp.select("a")
-#print(result)
-info = ""
-for item in result:
-	info += item.text + "\n\n"
-print(info)
+result=sp.select(".team-box")
+
+for x in result:
+print(x.text)
+print(x.find("a").get("href"))
