30265c76572129eda3d3160591ddbe6868a9e85a
Zhi06 <samhjh666@gmail.com>
2023-11-29 21:24:58 +0800

    v40

--- index.py
+++ index.py
@@ -23,6 +23,7 @@ def index():
     homepage += "<a href=/books>精選圖書列表</a><br>"
     homepage += "<a href=/read>書名查詢</a><br>"
     homepage += "<a href=/spider>網路爬蟲抓取子青老師課程</a><br>"
+     homepage += "<br><a href=/movie>讀取開眼電影即將上映影片，寫入Firestore</a><br>"
     return homepage
 
 
@@ -79,6 +80,83 @@ def spider():
         info += "<a href=" + x.find("a").get("href") + ">" + x.text +"</a><br>"
         info += x.find("a").get("href") + "<br><br>"
     return info
+
+@app.route("/movie")
+def movie():
+
+    url = "http://www.atmovies.com.tw/movie/next/"
+
+    Data = requests.get(url)
+
+    Data.encoding = "utf-8"
+
+    sp = BeautifulSoup(Data.text, "html.parser")
+
+    result=sp.select(".filmListAllX li")
+
+    lastUpdate = sp.find("div", class_="smaller09").text[5:]
+
+    for item in result:
+
+        picture = item.find("img").get("src").replace(" ", "")
+
+        title = item.find("div", class_="filmtitle").text
+
+        movie_id = item.find("div", class_="filmtitle").find("a").get("href").replace("/", "").replace("movie", "")
+
+        hyperlink = "http://www.atmovies.com.tw" + item.find("div", class_="filmtitle").find("a").get("href")
+
+        show = item.find("div", class_="runtime").text.replace("上映日期：", "")
+
+        show = show.replace("片長：", "")
+
+        show = show.replace("分", "")
+
+        showDate = show[0:10]
+
+        showLength = show[13:]
+
+    doc = {
+
+        "title": title,
+
+        "picture": picture,
+
+        "hyperlink": hyperlink,
+
+        "showDate": showDate,
+
+        "showLength": showLength,
+
+        "lastUpdate": lastUpdate
+
+    }
+
+    db = firestore.client()
+
+    doc_ref = db.collection("電影").document(movie_id)
+
+    doc_ref.set(doc)
+
+    return "近期上映電影已爬蟲及存檔完畢，網站最近更新日期為：" + lastUpdate
+@app.route("/searchQ", methods=["POST","GET"])
+def searchQ():
+    if request.method == "POST":
+        MovieTitle = request.form["MovieTitle"]
+        info = ""
+        db = firestore.client()     
+        collection_ref = db.collection("電影")
+        docs = collection_ref.order_by("showDate").get()
+        for doc in docs:
+            if MovieTitle in doc.to_dict()["title"]: 
+                info += "片名：" + doc.to_dict()["title"] + "<br>" 
+                info += "影片介紹：" + doc.to_dict()["hyperlink"] + "<br>"
+                info += "片長：" + doc.to_dict()["showLength"] + " 分鐘<br>" 
+                info += "上映日期：" + doc.to_dict()["showDate"] + "<br><br>"           
+        return info
+    else:  
+        return render_template("input.html")
+
 
 
 
