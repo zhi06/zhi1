4cb1a9ce1c0d9bcd5c98a55f3c950c9817edaa44
Zhi06 <samhjh666@gmail.com>
2023-12-11 21:35:06 +0800

    v49

--- index.py
+++ index.py
@@ -216,8 +216,21 @@ def webhook():
     req = request.get_json(force=True)
     # fetch queryResult from json
     action =  req.get("queryResult").get("action")
-    msg =  req.get("queryResult").get("queryText")
-    info = "動作：" + action + "； 查詢內容：" + msg
+    #msg =  req.get("queryResult").get("queryText")
+    #info = "動作：" + action + "； 查詢內容：" + msg
+    if (action == "rateChoice"):
+    rate =  req.get("queryResult").get("parameters").get("rate")
+    info = "我是謝仁翔開發的電影聊天機器人,您選擇的電影分級是：" + rate + "，相關電影：\n"
+    db = firestore.client()
+        collection_ref = db.collection("電影含分級")
+        docs = collection_ref.get()
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if rate in dict["rate"]:
+                result += "片名：" + dict["title"] + "\n"
+                result += "介紹：" + dict["hyperlink"] + "\n\n"
+        info += result
     return make_response(jsonify({"fulfillmentText": info}))
 
 
